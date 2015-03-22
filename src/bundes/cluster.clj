b67cdb72ccbb5d39701fa0c957fbe0cde10de45a
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-22 21:40:53 +0100

    cluster: check command context

--- src/bundes/cluster.clj
+++ src/bundes/cluster.clj
@@ -4,14 +4,20 @@
 
 (defmethod perform-effect :stop
   [{:keys [unit]}]
+  (when-not (= :daemon (:type unit))
+    (throw (ex-info "invalid unit type" {:unit (:id unit)})))
   (debug "stopping unit " (:id unit)))
 
 (defmethod perform-effect :start
   [{:keys [unit]}]
+  (when-not (= :daemon (:type unit))
+    (throw (ex-info "invalid unit type" {:unit (:id unit)})))
   (debug "starting unit " (:id unit)))
 
 (defmethod perform-effect :one-off
   [{:keys [unit]}]
+  (when-not (= :batch (:type unit))
+    (throw (ex-info "invalid unit type" {:unit (:id unit)})))
   (debug "one-off run for unit " (:id unit)))
 
 
