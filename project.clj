4d8a668dc2190511fdab121610270ff1876676cf
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-23 22:16:28 +0100

    support uberjars

--- project.clj
+++ project.clj
@@ -14,6 +14,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts [~lib-path]
+  :aot :all
   :main bundes.main
   :dependencies [[org.clojure/clojure       "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
