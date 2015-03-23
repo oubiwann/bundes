d9249ebe14c8a0e77ef26236a290a728e9213a91
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-23 17:50:27 +0100

    pull ticker from the system.

--- src/bundes/tick.clj
+++ src/bundes/tick.clj
@@ -4,13 +4,11 @@
             [bundes.effect         :refer [perform-effect]]
             [clojure.tools.logging :refer [debug info]]))
 
-(def cnj
-  "Our scheduler instance"
-  (delay (c/cronj :entries [])))
-
-(defn start-ticker!
+(defn create!
   []
-  (c/start! @cnj))
+  (let [ticker (c/cronj :entries [])]
+    (c/start! ticker)
+    ticker))
 
 (defn tick-handler
   [{:keys [id] :as unit}]
@@ -18,11 +16,11 @@
   (t/task id (fn [_ _] (perform-effect {:action :one-off :unit unit}))))
 
 (defmethod perform-effect :sched-add
-  [{:keys [unit sched]}]
+  [{:keys [ticker unit sched]}]
   (debug "adding schedule:" sched "for unit: " (:id unit))
-  (c/schedule-task @cnj (tick-handler unit) sched))
+  (c/schedule-task ticker (tick-handler unit) sched))
 
 (defmethod perform-effect :sched-del
-  [{:keys [unit sched]}]
+  [{:keys [ticker unit sched]}]
   (debug "removing schedule:" sched "for unit: " (:id unit))
-  (c/unschedule-task @cnj (:id unit)))
+  (c/unschedule-task ticker (:id unit)))
