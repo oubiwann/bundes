1a2200e68214af924d5fd74f53afec3b2bfc002b
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-24 18:20:46 +0100

    decisions: group similar actions

--- src/bundes/decisions.clj
+++ src/bundes/decisions.clj
@@ -11,62 +11,40 @@
   [old new k]
   (let [old                 (get old k {:status :stop})
         new                 (get new k {:status :stop})]
-    (match [(:type old)
-            (:type new)
+    (match [(:type old) (:type new)
             (= (:runtime old) (:runtime new))
             (= (:schedule old) (:schedule new))
-            (:status old)
-            (:status new)]
+            (:status old) (:status new)]
 
            ;; Basic status changes
            [_ _ _ _       :stop :stop]  []
-           [:daemon _ _ _ :start :stop] [{:action :stop
-                                          :unit   old}]
-           [:batch  _ _ _ :start :stop] [{:action :sched-del
-                                          :unit   old
-                                          :sched  (:schedule old)}]
+           [:daemon _ _ _ :start :stop] [{:action :stop      :unit   old}]
+           [:batch  _ _ _ :start :stop] [{:action :sched-del :unit   old}]
 
-           [_ :daemon _ _ :stop :start] [{:action :start
-                                          :unit   new}]
-           [_ :batch  _ _ :stop :start] [{:action :sched-add
-                                          :unit   new
-                                          :sched  (:schedule new)}]
+           [_ :daemon _ _ :stop :start] [{:action :start     :unit   new}]
+           [_ :batch  _ _ :stop :start] [{:action :sched-add :unit   new}]
 
            ;; If we got this far, both units are in status :start
 
            ;; Unit switches from batch to daemon
-           [:batch :daemon _ _ _ _]     [{:action :sched-del
-                                          :unit   old
-                                          :sched  (:schedule old)}
-                                         {:action :start
-                                          :unit   new}]
+           [:batch :daemon _ _ _ _]     [{:action :sched-del :unit   old}
+                                         {:action :start     :unit   new}]
 
            ;; Unit switches from batch to daemon
-           [:daemon :batch _ _ _ _]     [{:action :stop
-                                          :unit   new}
-                                         {:action :sched-add
-                                          :unit   new
-                                          :sched  (:schedule new)}]
+           [:daemon :batch _ _ _ _]     [{:action :stop      :unit   new}
+                                         {:action :sched-add :unit   new}]
 
            ;; Unit runtime changed
-           [_ :daemon false _ _ _]      [{:action :stop
-                                          :unit   old}
-                                         {:action :start
-                                          :unit   new}]
+           [_ :daemon false _ _ _]      [{:action :stop      :unit   old}
+                                         {:action :start     :unit   new}]
 
            ;; Unit schedules changed
-           [_ :batch _ false _ _]       [{:action :sched-del
-                                          :unit   old
-                                          :sched  (:schedule old)}
-                                         {:action :sched-add
-                                          :unit   new
-                                          :sched  (:schedule new)}]
-
+           [_ :batch _ false _ _]       [{:action :sched-del :unit   old}
+                                         {:action :sched-add :unit   new}]
 
            ;; We really should not get this far
            [_ _ _ _ _ _]                (throw (ex-info "unhandled case"
-                                                        {:old old
-                                                         :new new})))))
+                                                        {:id [old new]})))))
 
 (defn add-map
   "If a new unit is in start state, return
@@ -75,11 +53,8 @@
   (let [val (get new k {})]
     (when (= :start (:status val))
       (if (= :batch (:type val))
-        {:action :sched-add
-         :sched (:schedule val)
-         :unit   val}
-        {:action :start
-         :unit   val}))))
+        {:action :sched-add :units [val]}
+        {:action :start     :units [val]}))))
 
 (defn del-map
   "If an old unit was in start state, return
@@ -88,11 +63,14 @@
   (let [val (get old k {})]
     (when (= :start (:status val))
       (if (= :batch (:type val))
-        {:action :sched-del
-         :sched (:schedule val)
-         :unit   val}
-        {:action :stop
-         :unit   val}))))
+        {:action :sched-del :units [val]}
+        {:action :stop      :units [val]}))))
+
+(defn group-effects
+  "Group similar effects together"
+  [[action effects]]
+  {:action action
+   :units  (map :unit effects)})
 
 (defn decisions
   "Given two versions of the world, return all resulting
@@ -110,4 +88,6 @@
           (map (partial add-map new) added)
           (map (partial del-map old) removed))
          (remove nil?)
-         (vec))))
+         (vec)
+         (group-by :action)
+         (map group-effects))))
