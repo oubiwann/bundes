673ed6916f31a5b5c8e73613f4ff3b1d0155ea2c
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-03-24 11:38:08 +0100

    ticker: provide mesos framework when forwarding action.

--- src/bundes/tick.clj
+++ src/bundes/tick.clj
@@ -15,14 +15,15 @@
 (defn tick-handler
   "When the scheduler ticks for a unit, just perform a new
    side-effect of type :one-off, which will be picked up by mesos"
-  [{:keys [id] :as unit}]
-  (debug "ticking for:" id)
-  (t/task id (fn [_ _] (perform-effect {:action :one-off :unit unit}))))
+  [payload]
+  (let [id (-> payload :unit :id)]
+    (debug "ticking for:" id)
+    (t/task id (fn [_ _] (perform-effect (assoc payload :action :one-off))))))
 
 (defmethod perform-effect :sched-add
-  [{:keys [ticker unit sched]}]
+  [{:keys [ticker unit sched] :as payload}]
   (debug "adding schedule:" sched "for unit: " (:id unit))
-  (c/schedule-task ticker (tick-handler unit) sched))
+  (c/schedule-task ticker (tick-handler payload) sched))
 
 (defmethod perform-effect :sched-del
   [{:keys [ticker unit sched]}]
