4b2061b13b522b8d666544347a1a28f3089e27fb
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-01 10:59:28 +0200

    mesos: small fixes

--- src/bundes/mesos.clj
+++ src/bundes/mesos.clj
@@ -5,25 +5,25 @@
             [mesomatic.types           :as t]
             [mesomatic.allocator       :refer [allocate-naively]]
             [bundes.effect             :refer [perform-effect]]
-            [clojure.core.async        :refer [chan <! go]]
+            [clojure.core.async        :refer [chan <! go] :as a]
             [clojure.tools.logging     :refer [debug info]]))
 
 (defmethod perform-effect :stop
   [{:keys [mesos units]}]
   (info "asked to start units")
-  (put! mesos {:type :stop :units units}))
+  (a/put! mesos {:type :stop :units units}))
 
 (defmethod perform-effect :start
   [{:keys [mesos units]}]
   (info "asked to stop units")
-  (put! mesos {:type :start :units units}))
+  (a/put! mesos {:type :start :units units}))
 
 (defmethod perform-effect :one-off
   [{:keys [mesos unit]}]
   (when-not (= :batch (:type unit))
     (throw (ex-info "invalid unit type" {:unit (:id unit)})))
   (debug "one-off run for unit " (:id unit))
-  (put! mesos {:type :start :units [unit]}))
+  (a/put! mesos {:type :start :units [unit]}))
 
 (defn unit->task-info
   "Convert a bundesrat unit into a suitable Mesos TaskInfo"
@@ -37,7 +37,7 @@
   (info "start: " (pr-str payload))
   (when-let [tasks (allocate-naively offers (map unit->task-info units))]
     (doseq [[offer-id tasks] (group-by :offer-id tasks)]
-      (launch-tasks! driver offer-id tasks)))
+      (s/launch-tasks! driver offer-id tasks)))
   state)
 
 (defmethod update-state :resource-offers
@@ -46,17 +46,17 @@
   state)
 
 (defmethod update-state :stop
-  [{:keys [driver]} {:keys [units]}]
+  [state payload]
   (info "stop!!")
   state)
 
 (defmethod update-state :offer-rescinded
-  [{:keys [offers] :as state} payload]
+  [state payload]
   (info "offer rescinded!" (pr-str payload))
   state)
 
 (defmethod update-state :status-update
-  [{:keys [offers] :as state} payload]
+  [state payload]
   (info "status update: " (pr-str payload))
   state)
 
@@ -76,5 +76,5 @@
                                          :principal "bundesrat-framework"})
         driver    (s/scheduler-driver sched framework master)]
     (s/start! driver)
-    (go (async/reduce update-state {:driver driver} inut))
+    (a/reduce update-state {:driver driver} input)
     input))
