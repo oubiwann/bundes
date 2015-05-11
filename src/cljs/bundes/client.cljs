403724329bad5a8f60e42cdc40ce8a09de4ccb98
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-11 08:37:06 +0200

    client: fixture data for app-state

--- src/cljs/bundes/client.cljs
+++ src/cljs/bundes/client.cljs
@@ -6,17 +6,29 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
-;; define your app data so that it doesn't get over-written on reload
-
 (def routes
   "Applicate route-map"
   ["/"         views/unit-list
    "/unit/:id" views/unit-details])
 
 (defonce app-state
-  (atom {}))
+  (atom {:units {:console
+                 {:type :daemon,
+                  :profile {:mem 512.0, :cpus 0.5, :count 2, :maxcol 2},
+                  :runtime
+                  {:type :docker,
+                   :docker
+                   {:image "dockerfile/nginx", :port-mappings [{:container-port 80}]}},
+                  :id :console,
+                  :status :start},
+                 :open-metering
+                 {:type :batch,
+                  :schedule "/20 * * * * * *",
+                  :profile {:mem 256, :cpus 1.0},
+                  :runtime {:type :command, :command "/srv/cluster/foobar"},
+                  :id :open-metering,
+
+                  :status :start}}}))
 
 (let [router (router/init routes app-state)
       target {:target (. js/document (getElementById "app"))}]
