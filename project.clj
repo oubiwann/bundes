c33f51ce3174a45091e5223ef6c51ac0fbe713c5
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-05-12 17:24:41 +0200

    project: pull-in latest core.async

--- project.clj
+++ project.clj
@@ -30,6 +30,7 @@
 
                  ;; clojurescript deps
                  [org.clojure/clojurescript    "0.0-3211"]
+                 [org.clojure/core.async       "0.1.346.0-17112a-alpha"]
                  [org.omcljs/om                "0.8.8"]
                  [figwheel                     "0.3.1"]
                  [sablono                      "0.3.4"]
